ce3917cff62d647bfc1976367ae619c332cdc197
John Batista <jbatista247@proton.me>
2026-09-03 15:19:09 -0500

    Add SEO metadata and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://blue-hill-0f6e9ff1e.5.azurestaticapps.net',
+  integrations: [sitemap()],
+});
